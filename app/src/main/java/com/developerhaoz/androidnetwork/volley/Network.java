3c7a283f9ac3adc0152c7013aa081bb3b3673444
Haoz <developerhaon@gmail.com>
2018-02-02 19:29:15 +0800

    第一篇博客总算撸完，总体的流程搞定。

---
+++ app/src/main/java/com/developerhaoz/androidnetwork/volley/Network.java
@@ -0,0 +1,8 @@
+package com.developerhaoz.androidnetwork.volley;
+
+/**
+ * @author Haoz
+ * @date 2018/2/1.
+ */
+public interface Network {
+}
---
+++ app/src/main/java/com/developerhaoz/androidnetwork/volley/NetworkDispatcher.java
@@ -0,0 +1,29 @@
+package com.developerhaoz.androidnetwork.volley;
+
+/**
+ * @author Haoz
+ * @date 2018/2/1.
+ */
+public class NetworkDispatcher extends Thread {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

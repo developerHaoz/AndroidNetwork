d7641f4a549c59a8504c7f1c4ba18816649d5065
Haoz <developerhaon@gmail.com>
2018-02-15 10:25:07 +0800

    感觉自己像是一条咸鱼。

--- app/src/main/java/com/developerhaoz/androidnetwork/TestActivity.java
+++ app/src/main/java/com/developerhaoz/androidnetwork/TestActivity.java
@@ -3,14 +3,11 @@ package com.developerhaoz.androidnetwork;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import java.util.ArrayDeque;
-
 public class TestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-        ArrayDeque stack = new ArrayDeque();
     }
 }

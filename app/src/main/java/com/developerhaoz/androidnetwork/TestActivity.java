98a8d349f225ed2d4ac7c5debd79e9578af8ceec
Haoz <developerhaon@gmail.com>
2018-03-02 22:59:00 +0800

    终于等到了腾讯的电话，周一就是最关键的一个面试了，加油，希望能有一个满意的结果。

--- app/src/main/java/com/developerhaoz/androidnetwork/TestActivity.java
+++ app/src/main/java/com/developerhaoz/androidnetwork/TestActivity.java
@@ -4,13 +4,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 
+import java.util.Stack;
+
 
 public class TestActivity extends AppCompatActivity {
+
+    Stack mStack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
     }
 
     @Override
